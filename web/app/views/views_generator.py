f065e7430deaa948589d21f1559bf9d97420fc30
miguelemosreverte <miguellemos95@gmail.com>
2017-05-28 20:39:11 -0300

    After some bug fixing, the IP directions and ports of the containers where not correctly configured or stepped on the network created by the TTT_web project, now this project, TTT_reactive, is production ready, in a loose sense of the word.

--- web/app/views/views_generator.py
+++ web/app/views/views_generator.py
@@ -5,6 +5,6 @@ with open('app/views/MTTT_views/home.html', 'r') as myfile:
         f.write('\
                 @(form: Form[controllers.FormData])(implicit messages: play.i18n.Messages)\n\
                 @main("MTTT - Machine Translation Training Tool") {\n'
-                + data[data.index("<body>") + len("<body>"):data.index("</body>")]
+                + data[data.index("<body>") + len("<body>"):data.index("</body>")].replace("http://10.5.0.6:9000/","http://10.6.0.6:9000/")
                 + '\n}')
         #print data[data.index("<body>") + len("<body>"):data.index("</body>")]
